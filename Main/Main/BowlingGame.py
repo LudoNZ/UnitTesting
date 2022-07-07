c34a9a8490425f5d35492a450dcf417dbaebcf90
ludo <ludobourneville@gmail.com>
2022-07-08 11:07:03 +1200

    DocStrings added

--- Main/Main/BowlingGame.py
+++ Main/Main/BowlingGame.py
@@ -1,16 +1,38 @@
 class BowlingGame:
+    '''A Class to Run the Back-end of a Bowling game Scoring System
+    
+    Attributes
+    ----------
+    rolls : array int
+        an Array to store the number of pins dropped for each ball. 
+    '''
 
     def __init__(self):
+        """Constructor, creates new array for each game"""
         self.rolls=[]
 
     def roll(self,pins):
+        """Simulates a Ball roll in game.
+        
+        Control to ensure pins downed is within valid range"""
+        if pins > 10:
+            pins = 10
+        elif pins < 0:
+            pins = 0
+
         self.rolls.append(pins)
 
     def score(self):
+        """Manages Game scoring.
+
+        goes through rollIndex Array and calculates score of 
+        a complete game. 
+        Determines if Frame is Strike, Spare or Ordinary to
+        add Scores accordingly.
+        """
         result = 0
         rollIndex=0
         for frameIndex in range(10):
-            #if frameIndex in range(10):
             if self.isStrike(rollIndex):
                 result += self.strike_score(rollIndex)
                 rollIndex +=1
@@ -24,16 +46,21 @@ class BowlingGame:
 
     #return boolean is strike?
     def isStrike(self, rollIndex):
+        """Checks if current rolls[] index value equals 10 pins in the first ball of current frame == STRIKE"""
         return self.rolls[rollIndex] == 10
 
     def isSpare(self, rollIndex):
+        """Checks if sum of current + next rolls[] index values equal 10 pins for both balls of current frame == SPARE"""
         return self.rolls[rollIndex]+ self.rolls[rollIndex+1]==10
 
     def strike_score(self,rollIndex):
+        """Returns 10 + score for next 2 balls"""
         return  10+ self.rolls[rollIndex+1]+ self.rolls[rollIndex+2]
 
     def spareScore(self,rollIndex):
+        """Returns 10 + Score for first ball of next frame"""
         return  10+ self.rolls[rollIndex+2]
 
     def frameScore(self, rollIndex):
+        """returns Score for both balls in current Frame"""
         return self.rolls[rollIndex] + self.rolls[rollIndex + 1]
--- Main/Main/Main.py
+++ Main/Main/Main.py
@@ -3,21 +3,28 @@ import BowlingGame
 
 class TestBowlingGame(unittest.TestCase):
 
-    print("testing here...")
-
     def setUp(self):
+        """Called at the start of every Test, resets the Rolls array"""
         self.game = BowlingGame.BowlingGame()
 
+    def tearDown(self):
+        """called to the end of every test,  prints rolls[] values to output"""
+        print(self.game.rolls)
+        print("result= " + str(self.game.score()) + "\n")
+
     def test_GutterGame(self):
+        """simulate a Game with 100% gutter balls"""
         for i in range(0, 20):
             self.game.roll(0)
         assert self.game.score()==0
 
     def testAllOnes(self):
+        """simulate a game with each ball dropping one pin"""
         self.rollMany(1, 20)
         self.assertEqual(self.game.score(), 20)
 
     def testOneSpare(self):
+        """simulates a game with a Spare in the first frame and"""
         self.game.roll(5)
         self.game.roll(5)
         self.game.roll(3)
@@ -25,6 +32,7 @@ class TestBowlingGame(unittest.TestCase):
         assert self.game.score()==16
 
     def testOneStrike(self):
+        """simulates a game with a Strike in first round"""
         self.game.roll(10)
         self.game.roll(4)
         self.game.roll(3)
@@ -32,18 +40,31 @@ class TestBowlingGame(unittest.TestCase):
         assert  self.game.score()==24
 
     def testPerfectGame(self):
+        """Simulates a Strike for each ball roll of a game"""
         self.rollMany(10,12)
         assert self.game.score()==300
 
-    def testOneSpare(self):
+    def testALLSpares(self):
+        """Simulates a Game where every ball roll knocks down 5 pins, resulting in a Spare for each frame"""
         self.rollMany(5,21)
         assert self.game.score()==150
         
     def rollMany(self, pins, roll):
+        """simulate multiple ball rolls with specified number of pins dropped"""
         for i in range(roll):
             self.game.roll(pins)
 
+    def testTooFewPins(self):
+        """tests input below expected range"""
+        self.rollMany(-1,20)
+        assert self.game.score()==0
+    
+    def testTooManyPins(self):
+        """tests input above expected range"""
+        self.rollMany(11,22)
+        assert self.game.score()==300
+
 if __name__ == '__main__':
+    """run Unit Tests"""
     print('running tests')
     unittest.main()
-        
